8eeead4c328c617a7a4f17d738d042263448ceab
Augusto Sigolo <augustosigolo@precato.com.br>
2022-01-05 14:42:37 -0300

    :iphone: Feito menu superior para a versão mobile

---
+++ frontend/src/components/GlobalComponents/UpperMenu.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="mobileMenu">
+      <nav>
+        <div class="personal">
+          <img src="../../assets/photo.jpg" alt="Minha foto" class="photo">
+          <div class="textInfo">
+            <h2>Augusto Sigolo</h2>
+            <h4>Desenvolvedor web</h4>
+          </div>
+        </div>
+        <div class="menuIconBox">
+          <img
+            v-if="!state.showMenu"
+            src="../../assets/menuIconUnpressed.svg"
+            alt="menu icon"
+            class="menuIcon"
+            @click="toggleMenu()"
+          />
+          <img
+            v-else
+            src="../../assets/menuIconPressed.svg"
+            alt="menu icon"
+            class="menuIcon"
+            @click="toggleMenu()"
+          />
+        </div>
+      </nav>
+      <menu v-if="state.showMenu">
+        <hr/>
+        <div class="mobileMenuButtons">
+          <a href="/">
+            <div class="mobileMenuButton">
+              sigolo.dev
+            </div>
+          </a>
+          <a href="/solucoes">
+            <div class="mobileMenuButton">
+              Soluções
+            </div>
+          </a>
+          <a href="/portfolio">
+            <div class="mobileMenuButton">
+              Portfólio
+            </div>
+          </a>
+          <a href="/contato">
+            <div class="mobileMenuButton">
+              Contato
+            </div>
+          </a>
+        </div>
+      </menu>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      showMenu: false,
+    });
+
+    function toggleMenu() {
+      state.showMenu = !state.showMenu;
+    }
+    return {
+      state,
+      toggleMenu,
+    };
+  },
+};
+</script>
+
+<style>
+
+.mobileMenu{
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+}
+
+.mobileMenu nav {
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: space-around;
+  height: 100%;
+  max-width: 100vw;
+  padding: 0.7em;
+  background: #420149;
+}
+
+.mobileMenu nav .photo{
+  width: 63px;
+  height: 63px;
+  border-radius: 100px;
+  margin-right: 1em;
+  vertical-align: middle;
+}
+
+.mobileMenu nav .personal{
+  display: flex;
+  flex-direction: row;
+  margin-right: 1em;
+}
+
+.mobileMenu nav .textInfo{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  vertical-align: middle;
+}
+
+.mobileMenu nav .textInfo h2 {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  color: #00DE28;
+  margin-bottom: -0.01em;
+  line-height: 0px;
+}
+
+.mobileMenu nav .textInfo h4{
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 300;
+  color: #2B9B3F;
+  line-height: 0px;
+}
+
+.mobileMenu nav .menuIconBox{
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+}
+
+.mobileMenu nav a .menuIconBox{
+  width: 30px;
+  height: 30px;
+  vertical-align: middle;
+}
+
+.mobileMenu menu .mobileMenuButtons{
+  width: 90%;
+  align-self: center;
+}
+
+.mobileMenu menu{
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  margin-top: -10px;
+  padding: 0;
+  margin-bottom: -10px;
+  background: #420149;
+}
+
+.mobileMenu menu hr{
+  align-self: center;
+  width: 70vw;
+  border:none;
+  height: 3px;
+  background: #9900A9;
+}
+
+.mobileMenu menu a{
+  background: #2B9B3F;
+  margin: 5px;
+  margin-bottom: 10px;
+  height: 3em;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  position:static;
+  border: 3px solid #00FF2E;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.5em;
+  font-weight: 600;
+  color: #420149;
+  text-decoration: none;
+}
+
+.mobileMenu menu a:active{
+  border: none;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,125 +1,151 @@
 <template>
   <div class="Home">
-  <div class="depth">
-    <main>
-      <main-box class="sideBar"/>
-      <div class="content">
-        <section>
-          <h1>sigolo.dev</h1>
-          <h2>Um hub? Um currículo?</h2>
-          <hr/>
-          <div class="text">
-           <p>  Eu sou um programador desde que me lembro. Meu primeiro contato com programação
-            foi logo aos 10 anos,
-           quando vendo tutoriais de programação em C fazia programas de contabilidade doméstica,
-           leitura de arquivos
-           e todos os outros scripts básico que todos fazemos.</p>
-           <p>  Com o passar do tempo, me aprofundei nos estudos e hoje, trabalho como desenvolvedor
-          backend em uma empresa do setor financeiro e também curso engenharia da computação.</p>
-           <p>
-              O objetivo desse site é servir de hub para todas aplicações que desenvolvo no meu
-              tempo
-              livre e, de quebra, servir como uma espécie de currículo virtual, já que aqui ficará
-              gravado
-              todo meu progresso nos estudos do desenvolvimento web.
-           </p>
+    <!--Versão pra computador-->
+    <mq-responsive target="lg+" class="desktopVersion">
+      <div class="depth">
+        <main>
+          <main-box class="sideBar"/>
+            <div class="content">
+              <section>
+                <h1>sigolo.dev</h1>
+                <h2>Um hub? Um currículo?</h2>
+                <hr/>
+                <div class="text">
+                  <p>  Eu sou um programador desde que me lembro. Meu primeiro contato
+                       com programação
+                       foi logo aos 10 anos,
+                       quando vendo tutoriais de programação em C fazia programas de contabilidade
+                       doméstica,
+                       leitura de arquivos
+                       e todos os outros scripts básico que todos fazemos.
+                  </p>
+                  <p>  Com o passar do tempo, me aprofundei nos estudos e hoje,
+                       trabalho como desenvolvedor
+                       backend em uma empresa do setor financeiro e também curso engenharia da
+                       computação.
+                  </p>
+                  <p>
+                       O objetivo desse site é servir de hub para todas aplicações
+                       que desenvolvo no meu
+                       tempo
+                       livre e, de quebra, servir como uma espécie de currículo
+                       virtual, já que aqui ficará
+                       gravado
+                       todo meu progresso nos estudos do desenvolvimento web.
+                  </p>
+               </div>
+            </section>
           </div>
-        </section>
+        </main>
       </div>
-    </main>
-  </div>
-  </div>
-</template>
+    </mq-responsive>
+    <mq-responsive target="sm-" class="mobileVersion">
+      <upper-menu/>
+    </mq-responsive>
+    </div>
+    </template>
 
 <script>
+import { useMq, MqResponsive } from 'vue3-mq';
 import MainBox from '../components/GlobalComponents/MainBox.vue';
+import UpperMenu from '../components/GlobalComponents/UpperMenu.vue';
 
 export default {
   name: 'Home',
   components: {
     MainBox,
+    MqResponsive,
+    UpperMenu,
+  },
+  setup() {
+    const mq = useMq();
+    return {
+      mq,
+    };
   },
 };
 </script>
 
 <style>
-.depth{
-  background: #925F98;
-  position: static;
+
+.desktopVersion .depth{
+background: #925F98;
+position: static;
 }
-main {
-  display: grid;
-  grid-template-columns: 4fr 10fr;
-  grid-template-rows: 1fr;
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-  position: relative;
-  bottom: 10px;
-  left: 10px;
-  height: calc(100vh - 30px);
+.desktopVersion main {
+display: grid;
+         grid-template-columns: 4fr 10fr;
+         grid-template-rows: 1fr;
+         grid-column-gap: 0px;
+         grid-row-gap: 0px;
+position: relative;
+bottom: 10px;
+left: 10px;
+height: calc(100vh - 30px);
 }
 
-.sideBar{
+.desktopVersion .sideBar{
   grid-area: 1 / 1 / 2 / 2;
 }
 
-.content{
+.desktopVersion .content{
   grid-area: 1 / 2 / 2 / 3;
+  height: 100%;
 }
 
-p {
+.desktopVersion p {
   font-family: 'Roboto', sans-serif;
 }
 
-.content section{
-  background: #191919;
-  margin-left: 20px;
-  margin-top: 20px;
-  margin-right: 20px;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  height: calc(100vh - 8% - 20px);
+.desktopVersion .content section{
+background: #191919;
+            margin-left: 20px;
+            margin-top: 20px;
+            margin-right: 20px;
+            border-radius: 10px;
+display: flex;
+         flex-direction: column;
+         justify-content: center;
+         align-content: center;
+height: calc(100vh - 8% - 20px);
 }
 
-.content section h1{
+.desktopVersion .content h1{
   font-family: 'Montserrat', sans-serif;
-  font-size: 100px;
+  font-size: 5px;
   margin-top: 0;
   margin-bottom: 0;
   font-weight: 400;
-  color: #00DE28;
+  color:white;
 }
 
-.content section h2{
-  color: #2B9B3F;
-  font-family: 'Montserrat', sans-serif;
-  font-size: 30px;
-  font-weight: 300;
-  margin-top: -5px;
+.desktopVersion .content section h2{
+color: #2B9B3F;
+       font-family: 'Montserrat', sans-serif;
+       font-size: 30px;
+       font-weight: 300;
+       margin-top: -5px;
 }
 
-.content section hr{
-  width: 65%;
-  height: 5px;
-  background: #9900A9;
-  border: none;
-  margin-top: -10px;
+.desktopVersion .content section hr{
+width: 65%;
+height: 5px;
+background: #9900A9;
+border: none;
+        margin-top: -10px;
 }
 
-.content section .text{
-  width: 80%;
-  align-self: center;
-  justify-self: center;
-  text-align: justify;
-  text-justify: inter-word;
+.desktopVersion .content section .text{
+width: 80%;
+       align-self: center;
+       justify-self: center;
+       text-align: justify;
+       text-justify: inter-word;
 }
 
-.content section .text p{
+.desktopVersion .content section .text p{
   text-indent: 1.5em;
-  color: #ECDBBA;
+color: #ECDBBA;
 
 }
 </style>
